3f64eea6ec07e9d71613ba69e7840f05b2b45c0f
Anthony Anderson <anderson@mdhs.info>
2018-07-02 22:38:42 -0400

    got controlers to read now to get it to read from multiple
    Signed-off-by: Anthony Anderson <anderson@mdhs.info>

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate gilrs;
+
+use gilrs::{Gilrs, Button, Event, EventType};
+fn main() {
+
+    let mut gilrs = Gilrs::new().unwrap();
+
+    // Iterate over all connected gamepads
+    for (_id, gamepad) in gilrs.gamepads() {
+        println!("{} is {:?}", gamepad.name(), gamepad.power_info());
+    }
+
+    loop {
+        // Examine new events
+        while let Some(Event { id, event, time }) = gilrs.next_event() {
+            println!("{:?} New event from {}: {:?}", time, id, event);
+        }
+        
+    }
+
+}
